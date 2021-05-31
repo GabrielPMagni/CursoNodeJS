32508a1aa20a85d811cd2efdb9410ccf08aee4ad
Gabriel Peres Magni <gabriel.magni@mysmartclinic.com.br>
2021-05-31 01:37:22 -0300

    add users model & route

---
+++ model/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, unique: false, selected: false},
+    created: {type: Date, default: Date.now()}
+});
+
+module.exports = mongoose.model('User', UserSchema);
