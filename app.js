4c10b94121fd174c128d5650743bf04580ce6ef3
Gabriel Peres Magni <gabriel.magni@mysmartclinic.com.br>
2021-05-31 01:07:12 -0300

    add mongodb conn & body-parser

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 require('dotenv/config');
+const url = process.env.MONGODB_CONN_STRING;
+const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
+
+mongoose.connect(url, options);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('connected', () => {
+    console.log('Aplicação conectada com sucesso!');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Erro na conexão com o banco de dados: ${err}`)
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Aplicação desconectada do banco de dados!');
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 const indexRoute = require('./Routes/index');
 const usersRoute = require('./Routes/users');
 
